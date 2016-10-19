5f89afd4f7a940700c2175f71a99b302dffe6a15
= <=>
2016-10-19 07:13:26 +0700

    update code

--- JavaSpoj/src/test1/PTIT016D.java
+++ JavaSpoj/src/test1/PTIT016D.java
@@ -1,5 +1,7 @@
 package test1;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -10,31 +12,28 @@ public class PTIT016D {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String str[] = input.nextLine().split(" ");
-        String inp = input.nextLine();
-        int n = Integer.parseInt(str[0]);
-        int k = Integer.parseInt(str[1]);
+        int n = input.nextInt();
+        int k = input.nextInt();
+        long a[] = new long[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = input.nextInt();
+        }
+        System.out.println(gt(a, n, k));
 
-        System.out.println(sort(inp, n, k));
     }
 
-    public static int sort(String s, int n, int k) {
-        String inp[] = s.split(" ");
-        for (int i = 0; i < n - 1; i++) {
-            for (int m = i + 1; m < n; m++) {
-                if (Integer.parseInt(inp[i]) < Integer.parseInt(inp[m])) {
-                    String tmp = inp[i];
-                    inp[i] = inp[m];
-                    inp[m] = tmp;
-                }
-            }
+    public static long gt(long a[], int n, int k) {
+        // trả về một mảng sau khi đã sắp xếp
+        System.out.println(n);
+        System.out.println(k);
+        Arrays.sort(a);
+        long tong = a[0];
+        int dauTru = n - k - 1;
+        for (int i = 1; i < dauTru + 1; i++) {
+            tong -= a[i];
         }
-        int tong = 0;
-        for (int i = 0; i < k + 1; i++) {
-            tong += Integer.parseInt(inp[i]);
-        }
-        for (int j = k + 1; j < n; j++) {
-            tong -= Integer.parseInt(inp[j]);
+        for (int j = dauTru +1; j < n; j++) {
+            tong += a[j];
         }
         return tong;
     }
